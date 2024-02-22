907c9ccca156a6615b1281746e011df1e3f6f255
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-02-22 17:03:16 +0200

    update

--- src/components/TripList/TripList.jsx
+++ src/components/TripList/TripList.jsx
@@ -1,70 +1,28 @@
-// import React from 'react';
-// import TripItem from '../TripItem/TripItem';
-// import { useTrips } from '../../hooks/useTrips';
-// import Button from '../Button/Button';
-
-// import { List, TripWrapper, Wrapper } from './TripList.styled';
-
-// const TripList = () => {
-//   const { trips, selectTrip, city } = useTrips();
-
-//   const getImageUrl = cityName => `./images/${cityName}.jpg`;
-
-//   const filteredTrips = city
-//     ? trips.filter(t => t.city.toLowerCase().includes(city.toLowerCase()))
-//     : trips;
-
-//   return (
-//     <Wrapper>
-//       <TripWrapper>
-//         <List>
-//           {filteredTrips?.map((trip, idx) => {
-//             return (
-//               <TripItem
-//                 key={idx}
-//                 onClick={() => {
-//                   selectTrip(trip.city);
-//                 }}
-//                 city={trip.city}
-//                 start={trip.startDate}
-//                 end={trip.endDate}
-//                 imgUrl={getImageUrl(trip.city.toLowerCase())}
-//               />
-//             );
-//           })}
-//         </List>
-//       </TripWrapper>
-//       <Button />
-//     </Wrapper>
-//   );
-// };
-
-// export default TripList;
-
-
 import React from 'react';
 import TripItem from '../TripItem/TripItem';
 import { useTrips } from '../../hooks/useTrips';
 import Button from '../Button/Button';
 import { List, TripWrapper, Wrapper } from './TripList.styled';
 
-// Імпортуйте зображення
 import londonImage from '../../images/london.jpg';
 import parisImage from '../../images/paris.jpg';
-// імпортуйте інші зображення за необхідності
+import berlinImage from '../../images/berlin.jpg';
+import romeImage from '../../images/rome.jpg';
+import pragaImage from '../../images/praga.jpg';
 
-// Створіть карту зображень
 const cityImages = {
   london: londonImage,
   paris: parisImage,
-  // додайте інші міста та їх зображення тут
+  berlin: berlinImage,
+  rome: romeImage,
+  praga: pragaImage,
 };
 
 const TripList = () => {
   const { trips, selectTrip, city } = useTrips();
 
   const getImageUrl = cityName =>
-    cityImages[cityName.toLowerCase()] || 'defaultImageURL';
+    cityImages[cityName.toLowerCase()];
 
   const filteredTrips = city
     ? trips.filter(t => t.city.toLowerCase().includes(city.toLowerCase()))
